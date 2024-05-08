8482f9252d0719c4de85f2e41798325d746b4907
jaeyong <ljy3573507@gmail.com>
2024-05-08 20:15:44 +0900

    URI 수정

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common"
 import { TodoService } from "./todo.service";
 import { CreateTodoDto } from "./dto";
 
-@Controller('api/v1/todo')
+@Controller('api/v1/todos')
 export class TodoController {
   constructor(private todoService: TodoService) {
   }
